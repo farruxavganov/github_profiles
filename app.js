f0a9956e143a3f0f8a5d812e831e70692fca1546
farrux <farrux.avganov@mail.ru>
2022-07-10 21:18:21 +0300

    finished project

---
+++ app.js
@@ -0,0 +1,48 @@
+const form = document.querySelector('.farm');
+const profile = document.querySelector(".profile");
+const value = document.querySelector(".input").value;
+
+const API = "https://api.github.com/users/";
+
+form.addEventListener("submit",function (e) {
+	e.preventDefault();
+	getUser(API);
+})
+
+getUser(API);
+async function getUser(API,value="farruxavganov") {
+	
+	if(value){
+		try{
+			const request = await fetch(API + value);
+			const response = await request.json();
+
+			let html = render(response);
+
+			profile.innerHTML = html;
+		}catch (e) {
+			console.log(e.message);
+		}
+	}
+}
+
+function render(response) {
+
+	return `<div class="profile__img">
+                  	<img src="${response.avatar_url}" alt="${response.login}" />
+                </div>
+                <div class="profile__infos">
+                    <h2 class="profile__title">${response.name}</h2>
+                    <h3 class="profile__job">${response.login}</h3>
+                    
+                    <p class="profile__disc">
+                        ${response.bio? response.bio : "Tasnifi qo'yilmagan"}
+                    </p>
+                    
+                   <ul class="profile__datas">
+                       <li class="profile__data eye"><i class="fa-solid fa-eye"></i> <span>${response.followers}</span></li>
+                       <li class="profile__data heart"><i class="fa-solid fa-heart"></i> <span>${response.following}</span></li>
+                       <li class="profile__data message"><i class="fa-solid fa-message"></i> <span>${response.public_repos}</span></li>
+                   </ul>
+                </div>`;
+}
